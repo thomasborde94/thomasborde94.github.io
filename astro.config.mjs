023fc45604cd0a867794e66ccae44b299d956686
thomasborde94 <thomas.borde94@hotmail.fr>
2023-12-11 16:03:33 +0100

    deleting

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://thomasborde94.github.io',
-    base: '/thomasborde94.github.io',
-});
