072d222efa3f7b7a50d52f1823d84f068cdccbe9
thomasborde94 <thomas.borde94@hotmail.fr>
2023-12-11 15:24:56 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thomasborde94.github.io',
+    base: '/thomasborde94.github.io',
 });
